1f4ba758d9cb602ff9df53f4a41a115f2a40beed
juaning <juaning@gmail.com>
2017-08-01 18:06:08 -0400

    Fix routing from camera to player

--- app/layouts/VideoPlayer/VideoPlayer.js
+++ app/layouts/VideoPlayer/VideoPlayer.js
@@ -1,26 +1,37 @@
-// import React, { Component } from 'react';
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { View } from 'react-native';
-// import Video from 'react-native-video';
+import Video from 'react-native-video';
 import styles from './styles';
 
-// class VideoPlayer extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Video
-//           source={this.props.videoSource}
-//           resizeMode="cover"
-//         />
-//       </View>
-//     );
-//   }
-// }
-
-const VideoPlayer = () => (
-  <View style={styles.container}>
-    <Text>Video Player</Text>
-  </View>
-);
+class VideoPlayer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isPlaying: false,
+    };
+    this.onLoadStart = this.onLoadStart.bind(this);
+  }
+  onLoadStart(e) {
+    this.setState({
+      isPlaying: true,
+    });
+    console.log('======> Playing video <======', e);
+  }
+  render() {
+    const { params } = this.props.navigation.state;
+    const { path } = params.data;
+    const uri = path.substring(0, path.length - 4);
+    return (
+      <View style={styles.container}>
+        <Video source={{ uri }} resizeMode="cover" onLoadStart={this.onLoadStart} />
+      </View>
+    );
+  }
+}
 
 export default VideoPlayer;
+
+VideoPlayer.propTypes = {
+  navigation: PropTypes.object,
+};
